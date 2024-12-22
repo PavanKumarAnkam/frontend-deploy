64d64ae3c28a79b6401a05aff9b41bd5117a5f16
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-12-22 13:14:24 +0530

    deploying

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.62.0"
+    }
+  }
+  backend "s3" {
+    bucket = "aws-s3-demo-pa1"
+    key = "expense-infra-dev-frontend-deploy"
+    region = "us-east-1"
+    dynamodb_table = "aws-dynamodb-demo-pa1"
+  }
+
+}
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
